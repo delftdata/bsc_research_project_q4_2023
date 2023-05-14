a82153b8963ae7969fd868f8c087a8961c4a45e6
Florena <i.buse@student.tudelft.nl>
2023-05-14 14:47:29 +0200

    Add Cramer's V correlation

--- correlation_based_feature_selection/src/correlation_methods/cramer.py
+++ correlation_based_feature_selection/src/correlation_methods/cramer.py
@@ -1,8 +1,14 @@
-import numpy as np
 from pandas import crosstab
 from scipy.stats import contingency
 
 
-def cramer_calculation(x, y):
-    obs4x2 = np.array([[100, 150], [203, 322], [420, 700], [320, 210]])
-    return contingency.association(x, method="cramer")
+class CramersVFeatureSelection:
+    @staticmethod
+    def compute_correlation(feature, target_feature):
+        contingency_table_values = crosstab(feature, target_feature).values
+        return contingency.association(contingency_table_values, method="cramer")
+
+    @staticmethod
+    def feature_selection(dataframe, target_feature):
+        # TODO: add the functionality
+        return dataframe, target_feature
--- correlation_based_feature_selection/src/run.py
+++ correlation_based_feature_selection/src/run.py
@@ -1,9 +1,12 @@
 from .correlation_methods.su import SymmetricUncertaintyFeatureSelection
+from .correlation_methods.cramer import CramersVFeatureSelection
 
 if __name__ == '__main__':
     print('hello')
 
-    a = [1, 2, 3]
-    b = [1, 2, 3]
+    # a = [1, 2, 3]
+    # b = [1, 2, 3]
+    a = ['a', 'b', 'c']
+    b = ['a', 'b', 'c']
     print(SymmetricUncertaintyFeatureSelection.compute_correlation(a, b))
-
+    print(CramersVFeatureSelection.compute_correlation(a, b))

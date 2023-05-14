75f09ac5f1078b86b86b1460dc61479c3778662b
Florena <i.buse@student.tudelft.nl>
2023-05-14 16:37:27 +0200

    Add the required requirements

--- correlation_based_feature_selection/src/run.py
+++ correlation_based_feature_selection/src/run.py
@@ -1,12 +1,14 @@
-from .correlation_methods.su import SymmetricUncertaintyFeatureSelection
+from .correlation_methods.pearson import PearsonFeatureSelection
+from .correlation_methods.spearman import SpearmanFeatureSelection
 from .correlation_methods.cramer import CramersVFeatureSelection
+from .correlation_methods.su import SymmetricUncertaintyFeatureSelection
 
 if __name__ == '__main__':
-    print('hello')
-
-    # a = [1, 2, 3]
-    # b = [1, 2, 3]
-    a = ['a', 'b', 'c']
-    b = ['a', 'b', 'c']
-    print(SymmetricUncertaintyFeatureSelection.compute_correlation(a, b))
+    a = [1, 2, 3, 3]
+    b = [1000, 7, 6, 1000]
+    # a = ['a', 'b', 'c', 'a', 'a', 'b']
+    # b = ['ddfdd', 'dsdfse', 'sdfsdf', 'sdfsf', 'coobbbi', 'coobbbi']
+    print(PearsonFeatureSelection.compute_correlation(a, b))
+    print(SpearmanFeatureSelection.compute_correlation(a, b))
     print(CramersVFeatureSelection.compute_correlation(a, b))
+    print(SymmetricUncertaintyFeatureSelection.compute_correlation(a, b))

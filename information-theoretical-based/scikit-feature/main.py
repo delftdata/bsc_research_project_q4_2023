1efc297e69722f0d82a8e4c3ebab862e519207a6
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2023-05-29 21:19:25 +0200

    Add SVM capabilities

--- information-theoretical-based/scikit-feature/main.py
+++ information-theoretical-based/scikit-feature/main.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 from autogluon.tabular import TabularDataset, TabularPredictor
 from autogluon.features.generators import AutoMLPipelineFeatureGenerator
 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR, SVC
 
 from warnings import filterwarnings
 import logging
@@ -43,7 +46,92 @@ def perform_feature_selection_all(n_features, train, y_label):
     return cife_result, jmi_result, mifs_result, mrmr_result
 
 
-def evaluate_model(train, test, fs_results, y_label, algorithms, hyperparameters, n_features):
+def evaluate_SVM(train, test, fs_results, y_label):
+    if train[y_label].nunique() > 20:
+        is_regression = True
+    else:
+        is_regression = False
+
+    result = []
+    for idx, duration in fs_results:
+        # obtain the dataset on the selected features
+        idx = list(set(idx))
+        n_features = len(idx)
+        picked_columns = [list(train.drop(y_label, axis=1).columns)[i] for i in idx[0:n_features]]
+
+        # Train model on the smaller dataset with tuned hyperparameters
+        if is_regression:
+            linear_predictor = SVR()
+        else:
+            linear_predictor = SVC()
+
+        linear_predictor.fit(train[picked_columns], train[y_label])
+        # Get accuracy on test data
+        test_data = TabularDataset(test[picked_columns])
+        prediction = linear_predictor.predict(test_data)
+
+        if is_regression:
+            score = np.sqrt(mean_squared_error(test[y_label], prediction))
+        else:
+            score = accuracy_score(test[y_label], prediction)
+        print(score)
+        result.append((score, duration))
+
+    return result
+
+
+def evaluate_performance_SVM():
+    with open('results/me.txt', "r") as file:
+        data = file.readlines()
+
+    data = [data[i:i + 5] for i in range(0, len(data), 5)]
+    for i in range(len(data)):
+        dataset = data[i]
+
+        mrmr_result = literal_eval(dataset[1].replace('array', ''))
+        mifs_result = literal_eval(dataset[2].replace('array', ''))
+        jmi_result = literal_eval(dataset[3].replace('array', ''))
+        cife_result = literal_eval(dataset[4].replace('array', ''))
+
+        mat = pd.read_csv([x['path'] for x in datasets if x['path'] == '../.' + dataset[0].strip()][0])
+        y_label = [x['y_label'] for x in datasets if x['path'] == '../.' + dataset[0].strip()][0]
+
+        train_data = TabularDataset(mat)
+        train_data = AutoMLPipelineFeatureGenerator(enable_text_special_features=False,
+                                                    enable_text_ngram_features=False).fit_transform(X=train_data)
+        train_data.fillna(0, inplace=True)
+        columns = train_data.columns
+        train_data = pd.DataFrame(MinMaxScaler((-1, 1)).fit_transform(train_data), columns=columns)
+        logging.error(dataset[0])
+        print(dataset[0])
+
+        # Split data
+        X_train, X_test, y_train, y_test = train_test_split(train_data.drop(columns=[y_label]), train_data[y_label],
+                                                            test_size=0.2, random_state=42)
+        train = X_train.copy()
+        train[y_label] = y_train
+        test = X_test.copy()
+        test[y_label] = y_test
+
+        # Perform evaluation
+        mrmr = evaluate_SVM(train, test, mrmr_result, y_label)
+        print(mrmr)
+        logging.error(mrmr)
+
+        mifs = evaluate_SVM(train, test, mifs_result, y_label)
+        print(mifs)
+        logging.error(mifs)
+
+        jmi = evaluate_SVM(train, test, jmi_result, y_label)
+        print(jmi)
+        logging.error(jmi)
+
+        cife = evaluate_SVM(train, test, cife_result, y_label)
+        print(cife)
+        logging.error(cife)
+
+
+def evaluate_model(train, test, fs_results, y_label, algorithms, hyperparameters):
     results = []
     for algorithm, hyperparameter in zip(algorithms, hyperparameters):
         result = []
@@ -61,7 +149,7 @@ def evaluate_model(train, test, fs_results, y_label, algorithms, hyperparameters
                 .fit(train_data=features, hyperparameters={algorithm: hyperparameter})
 
             # Get accuracy on test data
-            test_data = TabularDataset(test)
+            test_data = TabularDataset(test[picked_columns])
             accuracy = linear_predictor.evaluate(test_data)
             if 'accuracy' in accuracy:
                 accuracy = accuracy['accuracy']
@@ -95,10 +183,10 @@ def run_pipeline(mat, y_label, algorithm, model_name, n_features):
     cife_result, jmi_result, mifs_result, mrmr_result = perform_feature_selection_all(n_features, train, y_label)
 
     # Perform evaluation
-    mrmr = evaluate_model(train, test, mrmr_result, y_label, algorithm, hyperparameters, n_features)
-    mifs = evaluate_model(train, test, mifs_result, y_label, algorithm, hyperparameters, n_features)
-    jmi = evaluate_model(train, test, jmi_result, y_label, algorithm, hyperparameters, n_features)
-    cife = evaluate_model(train, test, cife_result, y_label, algorithm, hyperparameters, n_features)
+    mrmr = evaluate_model(train, test, mrmr_result, y_label, algorithm, hyperparameters)
+    mifs = evaluate_model(train, test, mifs_result, y_label, algorithm, hyperparameters)
+    jmi = evaluate_model(train, test, jmi_result, y_label, algorithm, hyperparameters)
+    cife = evaluate_model(train, test, cife_result, y_label, algorithm, hyperparameters)
 
     logging.error(mrmr)
     logging.error(mifs)
@@ -126,13 +214,39 @@ def visualize_results(dataset_name, model_names, n, mrmrs, mifss, jmis, cifes):
         plot_performance(dataset_name, n, mrmr_one, mifs_one, jmi_one, cife_one)
         i += 1
 
+def plot_feature_selection_performance_svm():
+    with open('results/logs/performance_complex_svm.txt', "r") as file:
+        data = file.readlines()
+
+    datasets = [data[i:i + 5] for i in range(0, len(data), 5)]
+    for i in range(len(datasets)):
+        dataset = datasets[i]
+        dataset_name = dataset[0].split('datasets/')[1].split('/')[0]
+        mrmr_result = literal_eval(dataset[1].replace('array', ''))
+        mifs_result = literal_eval(dataset[2].replace('array', ''))
+        jmi_result = literal_eval(dataset[3].replace('array', ''))
+        cife_result = literal_eval(dataset[4].replace('array', ''))
+
+        mrmr_one = [i[0] for i in mrmr_result]
+        mifs_one = [i[0] for i in mifs_result]
+        jmi_one = [i[0] for i in jmi_result]
+        cife_one = [i[0] for i in cife_result]
+
+        model_name = 'SVM'
+
+        plot_over_features(dataset_name, model_name, len(mrmr_one), mrmr_one, mifs_one, jmi_one, cife_one)
+
 
 def plot_feature_selection_performance():
-    with open('results/22-5-2023/performance_simple.txt', "r") as file:
-        data = file.readlines()
-    with open('results/22-5-2023/performance_complex.txt', "r") as file:
-        data_complex = file.readlines()
+    # with open('results/22-5-2023/performance_simple.txt', "r") as file:
+    #     data = file.readlines()
+    # with open('results/22-5-2023/performance_complex.txt', "r") as file:
+    #     data_complex = file.readlines()
 
+    with open('results/me.txt', "r") as file:
+        data = file.readlines()
+
+    data_complex = []
     datasets = [data[i:i + 5] for i in range(0, len(data), 5)]
     datasets_complex = [data_complex[i:i + 5] for i in range(0, len(data_complex), 5)]
     for i in range(len(datasets)):
@@ -280,7 +394,7 @@ def perform_feature_selection_for_multiple_datasets():
 
 
 def evaluate_performance():
-    with open('logs/all_logs.txt', "r") as file:
+    with open('results/logs/fs_simple.txt', "r") as file:
         data = file.readlines()
 
     data = [data[i:i + 5] for i in range(0, len(data), 5)]
@@ -292,8 +406,8 @@ def evaluate_performance():
         jmi_result = literal_eval(dataset[3].replace('array', ''))
         cife_result = literal_eval(dataset[4].replace('array', ''))
 
-        mat = pd.read_csv('../.' + [x['path'] for x in datasets if x['path'] == dataset[0].strip()][0])
-        y_label = [x['y_label'] for x in datasets if x['path'] == dataset[0].strip()][0]
+        mat = pd.read_csv([x['path'] for x in datasets if x['path'] == '../.' + dataset[0].strip()][0])
+        y_label = [x['y_label'] for x in datasets if x['path'] == '../.' + dataset[0].strip()][0]
         n_features = len(mat.columns) - 1
 
         algorithm = ['XGB', 'LR']
@@ -315,10 +429,10 @@ def evaluate_performance():
         test[y_label] = y_test
 
         # Perform evaluation
-        mrmr = evaluate_model(train, test, mrmr_result, y_label, algorithm, hyperparameters, n_features)
-        mifs = evaluate_model(train, test, mifs_result, y_label, algorithm, hyperparameters, n_features)
-        jmi = evaluate_model(train, test, jmi_result, y_label, algorithm, hyperparameters, n_features)
-        cife = evaluate_model(train, test, cife_result, y_label, algorithm, hyperparameters, n_features)
+        mrmr = evaluate_model(train, test, mrmr_result, y_label, algorithm, hyperparameters)
+        mifs = evaluate_model(train, test, mifs_result, y_label, algorithm, hyperparameters)
+        jmi = evaluate_model(train, test, jmi_result, y_label, algorithm, hyperparameters)
+        cife = evaluate_model(train, test, cife_result, y_label, algorithm, hyperparameters)
 
         print(mrmr)
         logging.error(mrmr)
@@ -331,7 +445,7 @@ def evaluate_performance():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='app.log', filemode='a', level=logging.ERROR, format='%(message)s')
+    # logging.basicConfig(filename='app.log', filemode='a', level=logging.ERROR, format='%(message)s')
 
     datasets = []
     datasets.append({'path': '../../datasets/bike-sharing/hour.csv', 'y_label': 'cnt', 'n_features': 15})
@@ -340,9 +454,13 @@ if __name__ == '__main__':
     datasets.append({'path': '../../datasets/breast-cancer/data.csv', 'y_label': 'diagnosis', 'n_features': 30})
     datasets.append({'path': '../../datasets/housing-prices/train.csv', 'y_label': 'SalePrice', 'n_features': 80})
     datasets.append({'path': '../../datasets/steel-plates-faults/steel_faults_train.csv', 'y_label': 'Class', 'n_features': 33})
-    # datasets.append({'path': '../../datasets/gisette/gisette_train.csv', 'y_label': 'Class', 'n_features': 250})
+    datasets.append({'path': '../../datasets/gisette/gisette_train.csv', 'y_label': 'Class', 'n_features': 250})
+    datasets.append({'path': '../../datasets/internet_advertisements/internet_advertisements.csv', 'y_label': 'class', 'n_features': 200})
 
     # perform_feature_selection_for_multiple_datasets()
     # evaluate_performance()
-    plot_feature_selection_runtime()
+    # plot_feature_selection_runtime()
     # plot_feature_selection_performance()
+    # plot_two_side_by_side()
+    # evaluate_performance_SVM()
+    plot_feature_selection_performance_svm()

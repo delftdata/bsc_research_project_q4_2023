8ba1f694b1d9b6b0497538dd807fd9348c744a3a
Florena Buse <i.buse@student.tudelft.nl>
2023-07-03 08:46:21 +0200

    Add all correlation methods

--- correlation_based_feature_selection/src/correlation_methods/information_gain.py
+++ correlation_based_feature_selection/src/correlation_methods/information_gain.py
@@ -1,13 +1,12 @@
 """
-Module for feature selection using Symmetric Uncertainty method.
+Module for feature selection using Information Gain method.
 """
-from .utility.entropy_estimators import calculate_entropy
 from .utility.mutual_information import calculate_information_gain
 
 
-class SymmetricUncertaintyFeatureSelection:
+class InformationGainFeatureSelection:
     """
-    Class to perform feature selection using the Symmetric Uncertainty method.
+    Class to perform feature selection using the Information Gain method.
 
     Methods
     -------
@@ -16,8 +15,8 @@ class SymmetricUncertaintyFeatureSelection:
     @staticmethod
     def compute_correlation(feature, target_feature):
         """
-        SELECT K BEST ALGORITHM: Calculates the correlation between the feature and target feature using the Symmetric
-        Uncertainty method. A value of 0 means that the features are independent, whereas a value
+        SELECT K BEST ALGORITHM: Calculates the correlation between the feature and target feature using the Information
+        Gain method. A value of 0 means that the features are independent, whereas a value
         of 1 means that knowledge of the feature’s value strongly represents target’s value. Source
         of the code is: https://github.com/jundongl/scikit-feature.
 
@@ -28,24 +27,15 @@ class SymmetricUncertaintyFeatureSelection:
 
         Returns
         -------
-        symmetric_uncertainty (float): Correlation between the two features measured using
-                                       Symmetric Uncertainty method
+        information_gain (float): Correlation between the two features measured using
+                                  Information Gain
         """
-        # Calculate information gain of feature and target
-        gain = calculate_information_gain(feature, target_feature)
-        # Calculate entropy of feature
-        feature_entropy = calculate_entropy(feature)
-        # Calculate entropy of target feature
-        target_feature_entropy = calculate_entropy(target_feature)
-        # Calculate the symmetric uncertainty between feature and target feature
-        symmetric_uncertainty = 2.0 * gain / (feature_entropy + target_feature_entropy)
-
-        return symmetric_uncertainty
+        return calculate_information_gain(feature, target_feature)
 
     @staticmethod
     def feature_selection(train_dataframe, target_feature, number_features):
         """
-        Performs feature selection using the Symmetric Uncertainty correlation-based method. Selects
+        Performs feature selection using the Information Gain correlation-based method. Selects
         a specified number of top-performing features.
 
         Parameters
@@ -56,25 +46,25 @@ class SymmetricUncertaintyFeatureSelection:
 
         Returns
         -------
-        selected_features (list): List of selected features using the Symmetric Uncertainty correlation
+        selected_features (list): List of selected features using Information Gain
         """
         target_column = train_dataframe[target_feature]
         train_dataframe = train_dataframe.drop(columns=[target_feature])
 
         # Calculate the Symmetric Uncertainty correlation between each feature and the target feature
-        su_correlations = train_dataframe\
-            .apply(func=lambda feature: SymmetricUncertaintyFeatureSelection.compute_correlation(feature, target_column),
+        ig_correlations = train_dataframe\
+            .apply(func=lambda feature: InformationGainFeatureSelection.compute_correlation(feature, target_column),
                    axis=0)
 
         # Select the top features with the highest correlation
-        sorted_correlations = su_correlations.sort_values(ascending=False)
+        sorted_correlations = ig_correlations.sort_values(ascending=False)
 
         return sorted_correlations[:number_features].index.tolist()
 
     @staticmethod
     def feature_selection_second_approach(train_dataframe, target_feature, threshold):
         """
-        SELECT ABOVE C ALGORITHM: Performs feature selection using the Symmetric Uncertainty correlation-based method.
+        SELECT ABOVE C ALGORITHM: Performs feature selection using Information Gain correlation-based method.
         Selects a number of features that have correlation with the target above a certain threshold.
 
         Parameters
@@ -85,19 +75,19 @@ class SymmetricUncertaintyFeatureSelection:
 
         Returns
         -------
-        selected_features (list): List of selected features based on the Symmetric Uncertainty correlation using
+        selected_features (list): List of selected features based on the Information Gain correlation using
         "Select above c"
         """
         target_column = train_dataframe[target_feature]
         train_dataframe = train_dataframe.drop(columns=[target_feature])
 
-        # Calculate the Symmetric Uncertainty correlation between each feature and the target feature
-        su_correlations = train_dataframe \
-            .apply(func=lambda feature: SymmetricUncertaintyFeatureSelection.
+        # Calculate the Information Gain correlation between each feature and the target feature
+        ig_correlations = train_dataframe \
+            .apply(func=lambda feature: InformationGainFeatureSelection.
                    compute_correlation(feature, target_column),
                    axis=0)
 
         # Select the features with the absolute correlation above the threshold
-        filtered_features = [feature for feature, correlation in su_correlations.items()
+        filtered_features = [feature for feature, correlation in ig_correlations.items()
                              if correlation >= threshold]
         return filtered_features
